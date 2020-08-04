dcf31a00821125bc116ae348185b72686cbfb0b7
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-08-04 22:41:42 +0900

    add

---
+++ abc166/a/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc166/a/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc166/a/src/main.rs
---
+++ abc166/a/target/rls/debug/deps/rust_template-85455bc06a4196d8.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc166/a/target/rls/debug/deps/rust_template-85455bc06a4196d8.rmeta: src/main.rs
+
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc166/a/target/rls/debug/deps/rust_template-85455bc06a4196d8.d: src/main.rs
+
+src/main.rs:
