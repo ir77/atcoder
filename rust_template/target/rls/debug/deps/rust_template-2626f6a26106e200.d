88b8ac8f3b1fc2c8af2bcc01ff3c60952fca4b35
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-08-15 12:20:08 +0900

    add

---
+++ rust_template/target/rls/debug/deps/rust_template-2626f6a26106e200.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/rust_template/target/rls/debug/deps/rust_template-2626f6a26106e200.rmeta: src/main.rs
+
+/Users/ucucmacmini/Workspace/code/contest/atcoder/rust_template/target/rls/debug/deps/rust_template-2626f6a26106e200.d: src/main.rs
+
+src/main.rs:
