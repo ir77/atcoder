f0f69d0bd118b6d5559f3a0407c7c1f71638d7f4
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2021-04-08 21:51:58 +0900

    add

---
+++ abc004/abc004_1/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc004/abc004_1/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc004/abc004_1/src/main.rs
---
+++ abc004/abc004_2/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc004/abc004_2/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc004/abc004_2/src/main.rs
