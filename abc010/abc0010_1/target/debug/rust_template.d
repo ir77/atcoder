60c623aa01bfbb5ea916e8ec7fb52767873dcc5d
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2021-09-01 21:27:41 +0900

    add

---
+++ abc010/abc0010_1/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc010/abc0010_1/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc010/abc0010_1/src/main.rs
---
+++ abc010/abc0010_2/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/abc010/abc0010_2/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc010/abc0010_2/src/main.rs
