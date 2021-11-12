04650c375ce2a8db778c2bcebfe9da02a852e0af
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2021-11-12 23:29:18 +0900

    wip

---
+++ abc015/abc0015_1/target/debug/deps/proconio-931398664c34d034.d
@@ -0,0 +1,11 @@
+/Users/ucuc/Workspaces/atcoder/abc015/abc0015_1/target/debug/deps/proconio-931398664c34d034.rmeta: /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/lib.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/marker.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/mod.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/line.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/once.rs
+
+/Users/ucuc/Workspaces/atcoder/abc015/abc0015_1/target/debug/deps/libproconio-931398664c34d034.rlib: /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/lib.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/marker.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/mod.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/line.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/once.rs
+
+/Users/ucuc/Workspaces/atcoder/abc015/abc0015_1/target/debug/deps/proconio-931398664c34d034.d: /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/lib.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/marker.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/mod.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/line.rs /Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/once.rs
+
+/Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/lib.rs:
+/Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/marker.rs:
+/Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/mod.rs:
+/Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/line.rs:
+/Users/ucuc/.cargo/registry/src/github.com-1ecc6299db9ec823/proconio-0.4.3/src/source/once.rs:
--- abc015/abc0015_1/target/debug/rust_template.d
+++ abc015/abc0015_1/target/debug/rust_template.d
@@ -1,1 +1,1 @@
-/Users/ucucmacmini/Workspace/code/contest/atcoder/abc015/abc0015_1/target/debug/rust_template: /Users/ucucmacmini/Workspace/code/contest/atcoder/abc015/abc0015_1/src/main.rs
+/Users/ucuc/Workspaces/atcoder/abc015/abc0015_1/target/debug/rust_template: /Users/ucuc/Workspaces/atcoder/abc015/abc0015_1/src/main.rs
