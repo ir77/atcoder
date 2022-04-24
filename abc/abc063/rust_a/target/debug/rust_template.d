85815828ca2493a05fb7727ed4ac66d0007f8714
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-04-24 16:03:46 +0900

    add

---
+++ abc/abc063/rust_a/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucuc/Workspaces/atcoder/abc/abc063/rust_a/target/debug/rust_template: /Users/ucuc/Workspaces/atcoder/abc/abc063/rust_a/src/main.rs
---
+++ abc/abc064/rust_b/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/ucuc/Workspaces/atcoder/abc/abc064/rust_b/target/debug/rust_template: /Users/ucuc/Workspaces/atcoder/abc/abc064/rust_b/src/main.rs
