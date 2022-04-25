006ba7a3270a96e9f94040585303a717dc1875a6
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-04-25 22:52:40 +0900

    add

---
+++ abc/abc065/rust_b/target/debug/deps/rust_template-e4932d43537ea0c2.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/code/contest/atcoder/rust_template/target/debug/deps/rust_template-e4932d43537ea0c2: src/main.rs
+
+/Users/ucucmacmini/Workspace/code/contest/atcoder/rust_template/target/debug/deps/rust_template-e4932d43537ea0c2.d: src/main.rs
+
+src/main.rs:
