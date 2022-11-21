8302a6f04ee6b590bd1aff2da8ab38d76f8e3ff8
yuchimur <yuchimur@yahoo-corp.jp>
2022-11-21 23:26:41 +0900

    add

---
+++ abc/abc113/rust_b/target/debug/deps/rust_template-89f6406ec11d3180.d
@@ -0,0 +1,5 @@
+/Users/yuchimur/Workspace/atcoder/abc/abc113/rust_b/target/debug/deps/rust_template-89f6406ec11d3180: src/main.rs
+
+/Users/yuchimur/Workspace/atcoder/abc/abc113/rust_b/target/debug/deps/rust_template-89f6406ec11d3180.d: src/main.rs
+
+src/main.rs:
---
+++ abc/abc113/rust_b/target/debug/rust_template.d
@@ -0,0 +1,1 @@
+/Users/yuchimur/Workspace/atcoder/abc/abc113/rust_b/target/debug/rust_template: /Users/yuchimur/Workspace/atcoder/abc/abc113/rust_b/src/main.rs
