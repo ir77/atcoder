017f70c21c69a2450591ad35c32f615cc169070a
yuchimur <yuchimur@yahoo-corp.jp>
2022-11-27 21:54:28 +0900

    add

--- rust_template/src/main.rs
+++ rust_template/src/main.rs
@@ -28,6 +28,51 @@ fn main() {
 }
 
 #[allow(dead_code)]
+trait Transpose<'a, Elem, Iter, T>
+    where
+        Elem: 'a,
+        Iter: IntoIterator<Item = &'a Elem>,
+        T: IntoIterator<Item = Iter>,
+{
+    fn transpose(self) -> Transposed<'a, Elem, Iter>;
+}
+
+#[allow(dead_code)]
+impl<'a, Elem, Iter, T> Transpose<'a, Elem, Iter, T> for T
+    where
+        Elem: 'a,
+        Iter: IntoIterator<Item = &'a Elem>,
+        T: IntoIterator<Item = Iter>,
+{
+    fn transpose(self) -> Transposed<'a, Elem, Iter> {
+        Transposed {
+            iters: self.into_iter().map(IntoIterator::into_iter).collect(),
+        }
+    }
+}
+
+#[allow(dead_code)]
+struct Transposed<'a, Elem, Iter>
+    where
+        Elem: 'a,
+        Iter: IntoIterator<Item = &'a Elem>,
+{
+    iters: Vec<Iter::IntoIter>,
+}
+
+#[allow(dead_code)]
+impl<'a, Elem, Iter> Iterator for Transposed<'a, Elem, Iter>
+    where
+        Elem: 'a,
+        Iter: IntoIterator<Item = &'a Elem>,
+{
+    type Item = Vec<&'a Elem>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iters.iter_mut().map(Iterator::next).collect()
+    }
+}
+
+#[allow(dead_code)]
 fn get_radian_from(degree: f64) -> f64 {
     (degree * PI) / 180.0
 }
--- rust_template/target/debug/rust_template.d
+++ rust_template/target/debug/rust_template.d
@@ -1,1 +1,1 @@
-/Users/ucuc/Workspaces/atcoder/rust_template/target/debug/rust_template: /Users/ucuc/Workspaces/atcoder/rust_template/src/main.rs
+/Users/yuchimur/Workspace/atcoder/rust_template/target/debug/rust_template: /Users/yuchimur/Workspace/atcoder/rust_template/src/main.rs
